0a3b02ce77c87a9f98e9f4c9624cd68a9f49cf56
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-07-14 18:45:06 -0300

    feat: adiciona exemplos de strings e escape de caracteres

--- variaveis.js
+++ variaveis.js
@@ -10,9 +10,28 @@ console.log(nome, idade, profissao)
 
 nome = "Richard Marlon Balestrim"
 idade = 46
+// nacionalidade = "Australiano" // Este trecho retorna erro, pois a constante não pode ser alterada
+
+// Concatenação 
+console.log("---------------------")
+console.log("Nome: " + nome + ", ", idade + ", Teste")
+
+
+// Interpolação
+console.log("---------------------")
+console.log(`Nome: ${nome}, Idade: ${idade}, Teste, Horário: ${new Date().toLocaleTimeString()}`)
+
+// Strings com aspas simples, duplas e backeticks (crases)
+const stringSimples = 'Eu sou "Richard"'
+const stringDuplas = "Eu sou 'Richard Marlon Balestrim'"
+const stringBacketicks = `Eu sou o "Richard" 'da Silva'`
+const stringEscape = "Eu sou o \"Richard Marlon Balestrim\""
+
 document.getElementById('variaveis').innerHTML = `
 Nome: ${nome} <br>
 Idade: ${idade} <br>
 Profissão: ${profissao} <br>
 Nacionalidade: ${nacionalidade} <br>
-`
+Horário: ${new Date().toLocaleTimeString()} <br>
+Mensagem com strings: ${stringEscape}
+ `
